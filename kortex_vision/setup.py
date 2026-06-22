a64969348601b29ee57e145436ade0efb099a1cc
Marcos <marcos.zuzuarregui@gmail.com>
2026-06-22 14:23:45 -0700

    fixing models build finally

--- kortex_vision/setup.py
+++ kortex_vision/setup.py
@@ -12,7 +12,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "launch"), glob("launch/*")),
-#        (os.path.join("share", package_name, "models"), glob("resources/*.pt")),),
+        (os.path.join("share", package_name, "models"), glob("resource/*.pt")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
